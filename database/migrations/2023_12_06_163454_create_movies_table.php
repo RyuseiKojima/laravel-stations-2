1691d3eed563032a22dff1abf11a48a86d54a48c
Ryusei Kojima <ryusei@Ryuseis-MacBook-Air.local>
2024-03-09 10:54:32 +0900

    0309

--- database/migrations/2023_12_06_163454_create_movies_table.php
+++ database/migrations/2023_12_06_163454_create_movies_table.php
@@ -6,36 +6,36 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateMoviesTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-      Schema::create('movies', function (Blueprint $table) {
-        $table->id();
-        $table->string('title')->uniqe();
-        $table->text('image_url')->comment('画像URL');
-        $table->integer('published_year')->comment('公開年');
-        $table->text('description')->comment('概要');
-        $table->tinyInteger('is_showing')->comment('上映中かどうか');
-        $table->unsignedBigInteger('genre_id');
-        $table->timestamps();
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('movies', function (Blueprint $table) {
+      $table->id();
+      $table->string('title')->uniqe();
+      $table->text('image_url')->comment('画像URL');
+      $table->integer('published_year')->comment('公開年');
+      $table->text('description')->comment('概要');
+      $table->tinyInteger('is_showing')->comment('上映中かどうか');
+      $table->unsignedBigInteger('genre_id');
+      $table->timestamps();
 
-        $table->unique(['title']);
+      $table->unique(['title']);
 
-        $table->foreign('genre_id')->references('id')->on('genres');
-      });
-    }
+      $table->foreign('genre_id')->references('id')->on('genres');
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('movies');
-    }
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('movies');
+  }
 }
